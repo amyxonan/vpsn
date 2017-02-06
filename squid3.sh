ff7f98e18697f88b1b690ba20d6dbb1d47cde113
amyxonan <amynan75@gmail.com>
2017-02-06 21:03:52 +0800

    Update squid3.sh

--- squid3.sh
+++ squid3.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 apt-get -y install squid3
-wget -O /etc/squid3/squid.conf "https://raw.githubusercontent.com/amyxonan/vpns/master/squid.conf"
+wget -O /etc/squid3/squid.conf "https://raw.githubusercontent.com/amyxonan/vpsn/master/squid.conf"
 sed -i $MYIP2 /etc/squid3/squid.conf;
 service squid3 restart
